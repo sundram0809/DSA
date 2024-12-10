41f4c1fdf87cc01b3e492c25d01064f311186596
Sundram Modanwal <146741782+sundram091@users.noreply.github.com>
2024-12-11 02:05:29 +0530

    some_problem_solved

--- LINKLIST/CLL/CLL1.cpp
+++ LINKLIST/CLL/CLL1.cpp
@@ -9,10 +9,66 @@ struct Node
 class CLL
 {
     private:
-    Node *start;
+    Node *last;
     public:
     CLL();
+    CLL(CLL&);
+    CLL& operator= (CLL &);
+    void insertFirst(int );
+    void insertLast(int);
+    void insertAfter(int);
+    Node* search(int);
+    void deleteFirst();
+    void deleteLast();
+    void deleteNode(Node*);
+    ~CLL();
     
 };
+CLL::CLL()
+{
+    last=NULL;
+}
+void CLL::insertFirst(int data)
+{
+     Node *n=new Node;
+     n->item=data;
+     if(last==NULL)
+    { 
+        n->next=n;
+        last=n;
+    }
+    else
+    {
+        n->next=last->next;
+        last->next=n;
+    }
+}
+void CLL::insertLast(int data)
+{
+    Node*n=new Node;
+    n->item=data;
+    if(last==NULL)
+    {
+        n->next=n;
+        last=n;
+    }
+    else
+    {
+        n->next=last->next;
+        last->next=n;
+        last=n;
+    }
+}
+Node* CLL::search(int data)
+{
+    Node *t;
+
+}
+void CLL::insertAfter(int data)
+{
+    Node *n=new Node;
+    n->item=data;
+
+}
 
 
